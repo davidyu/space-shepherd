0487031965bb0c723b1fecff04a6da06dc7ac150
David Yu <lewen.yu@gmail.com>
2016-02-21 00:40:43 -0800

    update dependencies for MySQL

--- secrets.py
+++ secrets.py
@@ -1,5 +1,8 @@
 import os
 
-DROPBOX_APP_KEY      = os.environ.get('DROPBOX_APP_KEY'      , '')
-DROPBOX_APP_SECRET   = os.environ.get('DROPBOX_APP_SECRET'   , '')
-FLASK_SECRET_KEY     = os.environ.get('FLASK_SECRET_KEY'     , '')
+DROPBOX_APP_KEY      = os.environ.get('DROPBOX_APP_KEY'              , '')
+DROPBOX_APP_SECRET   = os.environ.get('DROPBOX_APP_SECRET'           , '')
+FLASK_SECRET_KEY     = os.environ.get('FLASK_SECRET_KEY'             , '')
+MYSQL_USERNAME       = os.environ.get('MYSQL_SPACESHEPHERD_USERNAME' , '')
+MYSQL_PASSWORD       = os.environ.get('MYSQL_SPACESHEPHERD_PASSWORD' , '')
+MYSQL_DBNAME         = os.environ.get('MYSQL_SPACESHEPHERD_DB_NAME'  , '')
